a625c9fee8412df59227972d649fe46aa7d8a89d
Jiwon Kang <jiwonk42@gmail.com>
2017-04-07 10:07:56 -0700

    add memberService and inject it to member compoment

--- src/app/member/member.component.ts
+++ src/app/member/member.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Member } from '../member.model';
 import { Router } from '@angular/router';
+import { MemberService } from '../member.service';
 
 @Component({
   selector: 'app-member',
   templateUrl: './member.component.html',
-  styleUrls: ['./member.component.css']
+  styleUrls: ['./member.component.css'],
+  providers: [MemberService]
 })
-export class MemberComponent {
-
-  constructor(private router: Router){}
+export class MemberComponent implements OnInit {
 
   members: Member[];
+
+  constructor(private router: Router, private memberService: MemberService){}
+
+  ngOnInit() {
+    this.members = this.memberService.getMembers();
+  }
 
   goToProfilePage(clickedMember: Member) {
    this.router.navigate(['members', clickedMember.id]);
