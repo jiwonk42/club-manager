ac1157e304ef6dec7fa45552e6b3dbe3fa83ec90
Jiwon Kang <jiwonk42@gmail.com>
2017-04-07 10:23:48 -0700

    install Firebase and add API key credentials to api-keys.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
 import { MemberComponent } from './member/member.component';
 import { routing } from './app.routing';
 import { AboutComponent } from './about/about.component';
 import { MemberProfileComponent } from './member-profile/member-profile.component';
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
@@ -20,7 +28,8 @@ import { MemberProfileComponent } from './member-profile/member-profile.componen
     BrowserModule,
     FormsModule,
     HttpModule,
-    routing
+    routing,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
