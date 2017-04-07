4c521617e5f9b4323f6d5988ef4be8f748af823c
Jiwon Kang <jiwonk42@gmail.com>
2017-04-07 09:58:50 -0700

    move data from member component to mock-members

--- src/app/member/member.component.ts
+++ src/app/member/member.component.ts
@@ -11,11 +11,7 @@ export class MemberComponent {
 
   constructor(private router: Router){}
 
-  members: Member[] = [
-    new Member("Jiwon", "President", "Duties: ......, Joined Since: 1998", 1),
-    new Member("Rebecca", "Vice President", "Duties: ....., Joined Since: 1200", 2),
-    new Member("Erin", "Treasury", "Duties: ....., Joined Since: 2009", 3)
-  ];
+  members: Member[];
 
   goToProfilePage(clickedMember: Member) {
    this.router.navigate(['members', clickedMember.id]);
---
+++ src/app/mock-members.ts
@@ -0,0 +1,7 @@
+import { Member } from './member.model';
+
+export const MEMBERS: Member[] = [
+  new Member("Jiwon", "President", "Duties: ......, Joined Since: 1998", 1),
+  new Member("Rebecca", "Vice President", "Duties: ....., Joined Since: 1200", 2),
+  new Member("Erin", "Treasury", "Duties: ....., Joined Since: 2009", 3)
+];
