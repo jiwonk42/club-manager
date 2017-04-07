4a08f74b26bf6e8909ab5e340610aa03fdf19e80
Jiwon Kang <jiwonk42@gmail.com>
2017-04-07 10:15:09 -0700

    inject memberService to member-profile component and display individual member profile on the detail page

--- src/app/member-profile/member-profile.component.ts
+++ src/app/member-profile/member-profile.component.ts
@@ -2,22 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 import { Member } from '../member.model';
+import { MemberService } from '../member.service';
 
 @Component({
   selector: 'app-member-profile',
   templateUrl: './member-profile.component.html',
-  styleUrls: ['./member-profile.component.css']
+  styleUrls: ['./member-profile.component.css'],
+  providers: [MemberService]
 })
 export class MemberProfileComponent implements OnInit {
 
   memberId: number = null;
+  memberToDisplay: Member;
 
-  constructor(private route: ActivatedRoute, private location: Location) { }
+  constructor(private route: ActivatedRoute, private location: Location, private memberService: MemberService) { }
 
   ngOnInit() {
     this.route.params.forEach((urlParameters) => {
       this.memberId = parseInt(urlParameters['id']);
     });
+    this.memberToDisplay = this.memberService.getMemberById(this.memberId);
   }
 
 }
